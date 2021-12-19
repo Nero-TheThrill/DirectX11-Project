8b234102ff3dbfa3b38112758c51ebcf5f9de6a3
JinwooChoi Laptop <imjinwoo98@gmail.com>
2021-12-18 21:20:54 -0800

    DirectX 11 Project setting complete

---
+++ DirectX11Project/DirectX11Project/main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <d3dx11.h>
+#include <xnamath.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Filename: main.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "systemclass.h"
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+{
+	SystemClass* System;
+	bool result;
+
+
+	// Create the system object.
+	System = new SystemClass;
+	if (!System)
+	{
+		return 0;
+	}
+
+	// Initialize and run the system object.
+	result = System->Initialize();
+	if (result)
+	{
+		System->Run();
+	}
+
+	// Shutdown and release the system object.
+	System->Shutdown();
+	delete System;
+	System = 0;
+
+	return 0;
+}
