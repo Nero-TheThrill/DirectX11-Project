3b4538ac462378a5cba41f2e34bd4e4198c598c9
JinwooChoi Laptop <imjinwoo98@gmail.com>
2021-12-20 17:43:21 -0800

    able to initialize and shut down direct3d

--- DirectX11Project/DirectX11Project/Graphicsclass.h
+++ DirectX11Project/DirectX11Project/Graphicsclass.h
@@ -7,8 +7,7 @@
 
 //////////////
 // INCLUDES //
-//////////////
-#include <windows.h>
+#include "d3dclass.h"
 
 
 /////////////
@@ -38,7 +37,7 @@ private:
 	bool Render();
 
 private:
-
+	D3DClass* m_Direct3D;
 };
 
 #endif
